c415fc1d9bfef1c32f36b652e7f4a8991c4309b2
Edward du Plessis <EdwardDP@agilebridge.co.za>
2024-08-17 16:25:29 +0200

    rick

--- vue-hackathong-nuxt/pages/About.vue
+++ vue-hackathong-nuxt/pages/About.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about min-h-screen flex items-center justify-center">
+    <div class="w-full max-w-[560px] aspect-video">
+      <iframe 
+        class="w-full h-full"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1" 
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+      </iframe>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
+/* You can remove the style block if you're using only Tailwind classes */
 </style>
