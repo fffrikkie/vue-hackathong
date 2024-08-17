15cbe59ed82c95efe90a0eb29ef97d16e4d55a7a
Edward du Plessis <EdwardDP@agilebridge.co.za>
2024-08-17 16:30:17 +0200

    ricky

--- vue-hackathong-nuxt/pages/About.vue
+++ vue-hackathong-nuxt/pages/About.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="about min-h-screen flex items-center justify-center">
-    <div class="w-full max-w-[560px] aspect-video">
-      <iframe 
-        class="w-full h-full"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1" 
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-      </iframe>
+  <div class="min-h-screen  p-8">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div v-for="i in 20" :key="i" class="aspect-video">
+        <iframe 
+          class="w-full h-full rounded-lg shadow-lg"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=0" 
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+        </iframe>
+      </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-/* You can remove the style block if you're using only Tailwind classes */
-</style>
