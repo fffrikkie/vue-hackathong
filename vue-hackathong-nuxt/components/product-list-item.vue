a97e53569d0848e05257ed95bdafd81c197e72cc
Edward du Plessis <EdwardDP@agilebridge.co.za>
2024-08-17 12:18:16 +0200

    lekker malaka

--- vue-hackathong-nuxt/components/product-list-item.vue
+++ vue-hackathong-nuxt/components/product-list-item.vue
@@ -1,10 +1,26 @@
 <template>
-    <div class="product-item" @click=" navigateTo('/about')">
-        <img :src="product.imageUrl" :alt="product.name" class="product-image">
-        <div class="product-details">
-            <h3 class="text-red-500 text-base">{{ product.name }}</h3>
-            <p class="product-price">{{ product.price }}</p>
-            <button class="add-to-cart-button">Add to Cart</button>
+    <div
+        class="relative h-96 overflow-hidden rounded-xl bg-[#3d3c3d] hover:scale-105 transition-transform duration-300 ease-in-out shadow-xl">
+        <div
+            class="absolute inset-0.5 flex flex-col items-start justify-start text-white z-[1] rounded-xl bg-[#323132] bg-opacity-90">
+            <div class="w-full h-48 flex items-center justify-center overflow-hidden rounded-t-xl bg-white">
+                <img :src="product.imageUrl" :alt="product.name" class="w-full h-full object-contain">
+            </div>
+            <div class="p-4 w-full">
+                <h3 class="text-lg font-semibold mb-2 text-left">{{ product.name }}</h3>
+                <p class="mb-4 text-left">R {{ product.price }}</p>
+            </div>
+            <button class="group cursor-pointer outline-none hover:rotate-90 duration-300 absolute bottom-4 right-4"
+                title="Add to cart">
+                <svg class="w-8 h-8 stroke-teal-500 fill-none group-hover:fill-white-200 group-active:stroke-teal-200 group-active:fill-teal-600 group-active:duration-0 duration-300"
+                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-width="1.5"
+                        d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z">
+                    </path>
+                    <path stroke-width="1.5" d="M8 12H16"></path>
+                    <path stroke-width="1.5" d="M12 16V8"></path>
+                </svg>
+            </button>
         </div>
     </div>
 </template>
--- vue-hackathong-nuxt/pages/(product-list)/product-list.vue
+++ vue-hackathong-nuxt/pages/(product-list)/product-list.vue
@@ -1,7 +1,7 @@
 <template>
     <div >
         <div class="text-5xl p-8">Product List</div class="">
-        <div class="grid gap-4 grid-cols-3 p-8">
+        <div class="grid gap-8 md:grid-cols-1 lg:grid-cols-4 p-8">
             <div v-for="product in products" :key="product.id">
                 <ProductListItem :id="product.id"  :name="product.name" :price="product.price" :imageUrl="product.imageUrl" />
             </div>
@@ -14,11 +14,14 @@ import ProductListItem from '@/components/product-list-item.vue';
 
 
 const products = [
-    { id: 1, name: 'Product 1', price: 10, imageUrl: 'https://via.placeholder.com/150' },
-    { id: 2, name: 'Product 2', price: 20, imageUrl: 'https://via.placeholder.com/150' },
-    { id: 3, name: 'Product 3', price: 30, imageUrl: 'https://via.placeholder.com/150' },
-    { id: 3, name: 'Product 4', price: 40, imageUrl: 'https://via.placeholder.com/150' },
+    { id: 1, name: 'Product 1', price: 49, imageUrl: 'https://media.takealot.com/covers_images/7ee4f95677f14d23bd52b29a2d3bc387/s-pdpxl.file' },
+    { id: 2, name: 'GetUp Pro Boxing Mask', price: 399, imageUrl: 'https://media.takealot.com/covers_images/624bd5e138424b20a77c55d0b269c1a9/s-zoom.file' },
+    { id: 3, name: 'Call of Duty Black Ops 6 for PS5 - PRE ORDER', price: 1629, imageUrl: 'https://media.takealot.com/covers_images/ea1f0cbee8b74b9ebaf0dc93fbd5dda8/s-zoom.file' },
+    { id: 3, name: '3-in-1 Handheld Strong Suction Vacuum Cleaner 600W', price: 850, imageUrl: 'https://media.takealot.com/covers_images/dc7a8054062a4be0a515ecbc27875b1b/s-zoom.file' },
     { id: 3, name: 'Product 5', price: 50, imageUrl: 'https://via.placeholder.com/150' },
+    { id: 3, name: 'Product 6', price: 60, imageUrl: 'https://via.placeholder.com/150' },
+    { id: 3, name: 'Product 5', price: 50, imageUrl: 'https://via.placeholder.com/150' },
+    { id: 3, name: 'Product 6', price: 60, imageUrl: 'https://via.placeholder.com/150' },
     { id: 3, name: 'Product 6', price: 60, imageUrl: 'https://via.placeholder.com/150' },
 ];
 
