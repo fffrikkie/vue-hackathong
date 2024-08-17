89fa48c6aefd17692e341d329598ef5ed7828b53
Edward du Plessis <EdwardDP@agilebridge.co.za>
2024-08-17 08:52:00 +0200

    chore: Add product list component with initial data

---
+++ hackathon-vue/src/components/product-list/product-list.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <ul>
+            <li v-for="product in products" :key="product.id">
+                {{ product.name }} - {{ product.price }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            products: [
+                { id: 1, name: 'Product 1', price: 10 },
+                { id: 2, name: 'Product 2', price: 20 },
+                { id: 3, name: 'Product 3', price: 30 },
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #333;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+}
+</style>
